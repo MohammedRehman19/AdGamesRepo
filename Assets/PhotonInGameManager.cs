2b08a6d5007a2b6600d59649fb5cbf0bfa77cc2a
MohammedRehman19 <mrehmanb5@gmail.com>
2021-04-16 17:59:25 +0500

    First update

---
+++ Assets/PhotonInGameManager.cs
@@ -0,0 +1,64 @@
+﻿using Photon.Pun;
+using Photon.Pun.Demo.PunBasics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Realtime;
+using UnityEngine.UI;
+public class PhotonInGameManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        if (!PhotonNetwork.IsConnected) // 1
+        {
+            SceneManager.LoadScene("Launcher");
+            return;
+        }
+
+        
+               GameObject player1 = PhotonNetwork.Instantiate("Car",
+                  Vector3.zero,
+                   Quaternion.identity);
+            
+        
+    }
+    public override void OnPlayerEnteredRoom(Player other)
+    {
+      
+        //Debug.Log("OnPlayerEnteredRoom() " + other.NickName); // not seen if you're the player connecting
+
+        //if (PhotonNetwork.IsMasterClient)
+        //{
+        //    Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+        //    LoadArena();
+        //}
+
+
+    }
+    public override void OnPlayerLeftRoom(Player other)
+    {
+      GameObject.FindObjectOfType<Game>().AvaliabliabltyChecker(GameObject.FindObjectOfType<BoardPlayer>().myplayer == "white" ? 1 : 0,false);
+    }
+
+        void LoadArena()
+    {
+        //if (!PhotonNetwork.IsMasterClient)
+        //{
+        //    Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+        //}
+
+        //Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+
+        //PhotonNetwork.LoadLevel("Game");
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
